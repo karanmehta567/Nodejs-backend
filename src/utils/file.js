ff772e700d999b8b96f62640821f1cb183caab79
karanmehta567 <karanmehta29.km@gmail.com>
2025-04-16 00:03:37 +0530

    file upload done right

---
+++ src/utils/file.js
@@ -0,0 +1,24 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from 'fs'
+
+cloudinary.config({
+    cloud_name: process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret: process.env.api_secret,
+})
+export const fileUploadtoCloudianry = async (localFilePath) => {
+    try {
+        if (!localFilePath) {
+            return null;
+        }
+        //lets upload it
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: 'auto'
+        })
+        console.log('Success while uploading file to cloudinary', response.url)
+        return response
+    } catch (error) {
+        fs.unlinkSync(localFilePath)
+        return null;
+    }
+}
